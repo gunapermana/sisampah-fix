0bbd4e56103253a20509eede4cfac3edfc44478b
Gungwahada1 <gungwahada1@gmail.com>
2022-10-09 10:10:42 +0800

    nambah btn register untuk guest

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,10 +14,11 @@
             <div class="col-sm-6">
                 <img src="./images/home.png" class="img-fluid" alt="Home Image">
             </div>
-            {{-- <div class="">
-      <div class="pb-2 fs-5" >Belum punya akun? Yuk buat dulu!</div>
-      <a class=" btn btn-transparent btn-lg mb-5 border border-success btn-angkut " href="#" role="button">Registrasi</a>
-    </div> --}}
+            <div class="">
+                <div class="pb-2 fs-5">Belum punya akun? Yuk buat dulu!</div>
+                <a class=" btn btn-transparent btn-lg mb-5 border border-success btn-angkut " href="#"
+                    role="button">Registrasi</a>
+            </div>
         </div>
     </div>
 @endsection
