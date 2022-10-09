4d5ed53e79d8c7a7abd74c7eebfd371adca7f747
Gungwahada1 <gungwahada1@gmail.com>
2022-10-09 10:51:31 +0800

    tambah controller user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,47 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // For Guest
+    public function guest()
+    {
+        return view('guest/dashboard');
+    }
+    public function guest_contact()
+    {
+        return view('guest/contact');
+    }
+    public function guest_about()
+    {
+        return view('guest/about');
+    }
+    public function guest_news()
+    {
+        return view('guest/news');
+    }
+
+    // For User Login
+    public function user()
+    {
+        return view('user/dashboard');
+    }
+    public function user_contact()
+    {
+        return view('user/contact');
+    }
+    public function user_about()
+    {
+        return view('user/about');
+    }
+    public function user_news()
+    {
+        return view('user/news');
+    }
+
+    // For Login
+
+
+    // For Register
+
+
+    // For Logout
 }
