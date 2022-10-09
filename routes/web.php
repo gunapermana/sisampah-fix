1ff0443d54877a47fa988a9748b8b94f60a9e7fb
Gungwahada1 <gungwahada1@gmail.com>
2022-10-09 09:55:07 +0800

    coba by gungwah

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
