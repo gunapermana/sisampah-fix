e77e2ef9cdb4354f16dd0fab48a185a48b785da1
Gungwahada1 <gungwahada1@gmail.com>
2022-10-14 16:35:18 +0800

    buat table petugas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
