8e903aab61450ac1675f7fda053a47ff3be21bed
Gungwahada1 <gungwahada1@gmail.com>
2022-10-14 15:54:14 +0800

    modif sedikit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('main')
+@extends('layout.main')
 @section('title', 'Dashboard')
 
 @section('content')
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,34 +1,36 @@
 <nav class="navbar navbar-expand-lg bg-transparent">
-  <div class="container">
-    <a class="navbar-brand" href="#" style="font-family: 'Oswald', sans-serif">
-      <img src="./images/logo1.png" alt="" height="60px">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <div class="fs-4 fw-bolder pt-2">SISAMPAH<p class="fs-6 fw-normal">Website Pengelolaan Sampah Desa Saba</p></div>
-      
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-6 fw-semibold">
-        <li class="nav-item menu-nav px-2">
-          <a class="nav-link active" aria-current="page" href="/dashboard">HOME</a>
-        </li>
-        <li class="nav-item menu-nav px-2">
-          <a class="nav-link active" aria-current="page" href="/about">ABOUT</a>
-        </li>
-        <li class="nav-item menu-nav px-2">
-          <a class="nav-link active" aria-current="page" href="/contact">CONTACT</a>
-        </li>
-        <li class="nav-item menu-nav px-2">
-          <a class="nav-link active" aria-current="page" href="/news">NEWS</a>
-        </li>
-        <li class="nav-item menu-nav px-2">
-          <a class="nav-link active" aria-current="page" href="/poin">POIN</a>
-        </li>
-        <li class="nav-item menu-nav px-2">
-          <a class="nav-link active text-success" aria-current="page" href="#">LOGIN</a>
-        </li>
-      </ul>
+    <div class="container">
+        <a class="navbar-brand" href="#" style="font-family: 'Oswald', sans-serif">
+            <img src="./images/logo1.png" alt="" height="60px">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <div class="fs-4 fw-bolder pt-2">SISAMPAH<p class="fs-6 fw-normal">Website Pengelolaan Sampah Desa Saba</p>
+            </div>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 fs-6 fw-semibold">
+                <li class="nav-item menu-nav px-2">
+                    <a class="nav-link active" aria-current="page" href="/dashboard">HOME</a>
+                </li>
+                <li class="nav-item menu-nav px-2">
+                    <a class="nav-link active" aria-current="page" href="/about">ABOUT</a>
+                </li>
+                <li class="nav-item menu-nav px-2">
+                    <a class="nav-link active" aria-current="page" href="/contact">CONTACT</a>
+                </li>
+                <li class="nav-item menu-nav px-2">
+                    <a class="nav-link active" aria-current="page" href="/news">NEWS</a>
+                </li>
+                <li class="nav-item menu-nav px-2">
+                    <a class="nav-link active" aria-current="page" href="/poin">POIN</a>
+                </li>
+                <li class="nav-item menu-nav px-2">
+                    <a class="nav-link active text-success" aria-current="page" href="#">LOGIN</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
