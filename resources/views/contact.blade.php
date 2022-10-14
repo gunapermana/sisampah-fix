1b1ccf97822a340240246eee4a4d60ff0c303903
Gungwahada1 <gungwahada1@gmail.com>
2022-10-14 16:34:25 +0800

    benerin

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,17 +1,17 @@
-@extends('user.layout.main')
+@extends('layout.main')
 @section('title', 'Contact')
 
 @section('content')
-<div class="container container-home">
-  <div class="row align-items-center pt-5">
-    <div class="col-sm-6">
-      <h1 class="fs-1 mb-5 fw-bold">KONTAK KAMI</h1>
-      <p class="fs-5">WhatsApp : 081234567891</p>
-      <p class="fs-5">Email : sisampah@gmail.com</p>
+    <div class="container container-home">
+        <div class="row align-items-center pt-5">
+            <div class="col-sm-6">
+                <h1 class="fs-1 mb-5 fw-bold">KONTAK KAMI</h1>
+                <p class="fs-5">WhatsApp : 081234567891</p>
+                <p class="fs-5">Email : sisampah@gmail.com</p>
+            </div>
+            <div class="col-sm-6 pt-5">
+                <img src="./images/contact.png" class="img-fluid" alt="Home Image">
+            </div>
+        </div>
     </div>
-    <div class="col-sm-6 pt-5">
-      <img src="./images/contact.png" class="img-fluid" alt="Home Image">
-    </div>
-  </div>
-</div>
 @endsection
--- resources/views/poin.blade.php
+++ resources/views/poin.blade.php
@@ -1,23 +1,24 @@
-@extends('user.layout.main')
+@extends('layout.main')
 @section('title', 'Poin')
 
 @section('content')
-<div class="container container-home">
-  <div class="row align-items-center pt-5">
-    <div class="col-sm-6">
-      <h1 class="fs-1 mb-5 fw-bold">TOTAL POIN</h1>
-      <h2 class="fs-4 mb-0">Total poin anda saat ini...</h2>
-      <p class="fw-bold text-poin">1678</p>
+    <div class="container container-home">
+        <div class="row align-items-center pt-5">
+            <div class="col-sm-6">
+                <h1 class="fs-1 mb-5 fw-bold">TOTAL POIN</h1>
+                <h2 class="fs-4 mb-0">Total poin anda saat ini...</h2>
+                <p class="fw-bold text-poin">1678</p>
+            </div>
+            <div class="col-sm-6">
+                <img src="./images/poin.png" class="img-fluid" alt="Home Image">
+            </div>
+            <div class="">
+                <div class="pb-2 fs-6">Yuk tukar poin anda dengan produk <br>
+                    yang disediakan oleh Desa Saba
+                </div>
+                <a class=" btn btn-transparent btn-lg mb-5 border border-success fs-5 fw-semibold " href="#"
+                    role="button">Tukar</a>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-6">
-      <img src="./images/poin.png" class="img-fluid" alt="Home Image">
-    </div>
-    <div class="">
-      <div class="pb-2 fs-6">Yuk tukar poin anda dengan produk <br>
-        yang disediakan oleh Desa Saba
-      </div>
-      <a class=" btn btn-transparent btn-lg mb-5 border border-success fs-5 fw-semibold " href="#" role="button">Tukar</a>
-    </div>
-  </div>
-</div>
 @endsection
